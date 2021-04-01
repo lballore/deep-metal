db5f625d5100f69d37cf69a25d42c56a4ed1e0b7
Luca Ballore <luca.ballore@gmail.com>
2021-04-01 22:54:43 +0200

    fix: fix converter script

--- src/model/scripts/convert_to_tf_model.py
+++ src/model/scripts/convert_to_tf_model.py
@@ -1,9 +1,10 @@
 import os
 
-from transformers import TFGPT2Model
+from transformers import GPT2Config, TFGPT2LMHeadModel
 
 MODEL_PATH = f"{os.path.dirname(os.path.realpath(__file__))}/../deepmetal"
 
 if __name__ == "__main__":
-    tf_model = TFGPT2Model.from_pretrained(f"{MODEL_PATH}/", from_pt=True)
-    tf_model.save_pretrained(f"{MODEL_PATH}/")
+    config = GPT2Config.from_pretrained(MODEL_PATH)
+    tf_model = TFGPT2LMHeadModel.from_pretrained(f"{MODEL_PATH}/pytorch_model.bin", from_pt=True, config=config)
+    tf_model.save_pretrained(MODEL_PATH)
